4fbfac8360e0c1bc8a4e1c398ab4be82189d8616
Pedro Kielblock <p.kielblock123@gmail.com>
2024-01-12 07:19:13 -0300

    fix: version v2.1 - fixed layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,9 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}
-    	<SpeedInsights />
-    	<Analytics />	  
-      <body/>
+        <SpeedInsights />
+        <Analytics />
+      </body>
     </html>
   )
 }
