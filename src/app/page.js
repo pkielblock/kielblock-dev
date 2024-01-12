560b42515860ad6979994133d822f8fe575661c2
Pedro Kielblock <p.kielblock123@gmail.com>
2024-01-12 07:45:44 -0300

    feat: version v2.3

--- src/app/page.js
+++ src/app/page.js
@@ -84,7 +84,7 @@ export default function Home() {
         </section>
         <footer class="bg-white dark:bg-gray-950">
             <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-              <span class="text-sm text-gray-600 sm:text-center dark:text-gray-300">© 2024 <a href="https://kielblock.dev/" class="hover:underline">kielblockdev™</a>
+              <span class="text-sm text-gray-600 sm:text-center dark:text-gray-300">2024 <a href="https://kielblock.dev/" class="hover:underline">kielblock.dev</a>
             </span>
             <ul class="flex flex-wrap  items-center mt-3 text-sm font-medium text-gray-600 dark:text-gray-300 sm:mt-0">
                 <li>
