44d813c37fd7099a01e8e41a85e709ef2ff797a1
Pedro Kielblock <p.kielblock123@gmail.com>
2024-01-12 07:16:15 -0300

    feat: version v2.1 - add analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,8 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    <SpeedInsights />
+      <body className={inter.className}>{children}
+    	<SpeedInsights />
+    	<Analytics />	  
+      <body/>
     </html>
   )
 }
