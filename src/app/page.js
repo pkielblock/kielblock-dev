31e43960f7250c954d308b46d847208fd32ae8ba
Pedro Kielblock <p.kielblock123@gmail.com>
2024-01-18 10:35:44 -0300

    feat: changed typing speed

--- src/app/page.js
+++ src/app/page.js
@@ -24,14 +24,18 @@ export default function Home() {
             </h1>
             <ul className='flex items-center'>
               <li>
-                  <a href='https://drive.google.com/uc?export=download&id=1_dbdBVcPezmVT1ccUFJHedXzFzxMqGmu' download="pedro_resume.pdf" class="hover:underline bg-blue-400 p-1 rounded-sm me-4 md:me-6 dark:text-white">Resume</a>
+                  <a href='https://drive.google.com/uc?export=download&id=1_dbdBVcPezmVT1ccUFJHedXzFzxMqGmu' 
+                  download="pedro_resume.pdf" class="hover:underline bg-blue-400 p-1 rounded-sm me-4 md:me-6 
+                  dark:text-white">Resume</a>
               </li>
               <li >
                 {
                   darkMode ? 
-                  <MdLightMode onClick={() => setDarkMode(!darkMode)} className='hover:scale-110 transition duration-250 cursor-pointer text-2xl dark:text-white'/> 
+                  <MdLightMode onClick={() => setDarkMode(!darkMode)} className='hover:scale-110 transition duration-250 
+                  cursor-pointer text-2xl dark:text-white'/> 
                   : 
-                  <MdDarkMode onClick={() => setDarkMode(!darkMode)} className='hover:scale-110 transition duration-250 cursor-pointer text-2xl'/>
+                  <MdDarkMode onClick={() => setDarkMode(!darkMode)} className='hover:scale-110 transition duration-250 
+                  cursor-pointer text-2xl'/>
                 } 
               </li>
             </ul>
@@ -49,17 +53,21 @@ export default function Home() {
                 words={['Software Developer']}
                 cursor
                 cursorStyle='_'
-                typeSpeed={100}
+                typeSpeed={300}
               />
             </h3>
-            <p className='text-md py-5 leading-8 text-gray-600 md:text-xl max-w-xl mx-auto md:leading-10  dark:text-gray-300'>
+            <p className='text-md py-5 leading-8 text-gray-600 md:text-xl max-w-xl mx-auto md:leading-10  
+            dark:text-gray-300'>
               Hey 👋 I am a passionate developer dedicated to continuous improvement and growth in my field. 
               Each day presents a new opportunity to enhance my skills and contribute to innovative solutions. 😎
             </p>
             <div className='text-5xl flex justify-center gap-16 py-20 text-gray-950  dark:text-white'>
-              <a href='https://github.com/pkielblock' target='_blank'><AiFillGithub className='hover:scale-110 transition duration-250 '/></a>
-              <a href='https://www.linkedin.com/in/pedrokielblock/' target='_blank'><AiFillLinkedin className='hover:scale-110 transition duration-250'/></a>
-              <a href='mailto:pedro@kielblock.dev' target='_blank'><AiFillMail className='hover:scale-110 transition duration-250'/></a>
+              <a href='https://github.com/pkielblock' target='_blank'><AiFillGithub className='hover:scale-110 
+              transition duration-250 '/></a>
+              <a href='https://www.linkedin.com/in/pedrokielblock/' target='_blank'><AiFillLinkedin 
+              className='hover:scale-110 transition duration-250'/></a>
+              <a href='mailto:pedro@kielblock.dev' target='_blank'><AiFillMail className='hover:scale-110 transition 
+              duration-250'/></a>
             </div>
             
           </div>
@@ -69,11 +77,19 @@ export default function Home() {
             <h3 id='background' className='text-3xl py-1 mb-5 text-center  dark:text-white'>
               Background
             </h3>
-            <p className='text-md py-2 leading-8 text-gray-600 md:text-xl md:leading-10 max-w-xl mx-auto  dark:text-gray-300'>
-              I hold a diploma in Systems Development from ETEC Lauro Gomes and a degree in Analysis and Systems Development from FATEC São Caetano do Sul - Antonio Russo. 
-              This dual background provides a comprehensive understanding of the technical and theoretical aspects of the field. 
-              My fluency in English is certified by <span className='font-bold  dark:text-white'>B2 First</span> and <span className='font-bold  dark:text-white'>C1 Advanced</span> qualifications, enabling effective communication in diverse professional settings. 
-              Currently, I am leveraging these skills in my role as a Systems Analyst at  <a className='font-bold text-blue-400 hover:underline hover:text-blue-700' href='https://www.rethinkingworks.com/' target='_blank'>ReThinkingWorks</a>, where I engage in a range of innovative projects, 
+            <p className='text-md py-2 leading-8 text-gray-600 md:text-xl md:leading-10 max-w-xl mx-auto  
+            dark:text-gray-300'>
+              I hold a diploma in Systems Development from ETEC Lauro Gomes and a degree in Analysis and 
+              Systems Development from FATEC São Caetano do Sul - Antonio Russo. 
+              This dual background provides a comprehensive understanding of the technical and theoretical 
+              aspects of the field. 
+              My fluency in English is certified by <span className='font-bold  dark:text-white'>B2 First</span> 
+              and <span className='font-bold  dark:text-white'>C1 Advanced</span> qualifications, 
+              enabling effective communication in diverse professional settings. 
+              Currently, I am leveraging these skills in my role as a Systems Analyst at  
+              <a className='font-bold text-blue-400 hover:underline hover:text-blue-700' 
+              href='https://www.rethinkingworks.com/' target='_blank'>ReThinkingWorks</a>, 
+              where I engage in a range of innovative projects, 
               applying my technical expertise and analytical skills to develop and optimize systems, 
               keeping pace with the rapidly evolving technological landscape.
             </p>
@@ -83,22 +99,31 @@ export default function Home() {
               Stack
             </h3>
             <div className='text-5xl flex justify-center gap-10 py-3 text-gray-950  dark:text-white'>
-              <div className='p-1 hover:scale-110 transition duration-250'><FaJava/><span className='pl-2 text-sm'>Java</span></div>
-              <div className='p-1 hover:scale-110 transition duration-250'><BiLogoSpringBoot/><span className='pl-1 text-sm'>Spring</span></div>
-              <div className='p-1 hover:scale-110 transition duration-250'><DiMysql/><span className='text-sm pl-1'>MySQL</span></div>
-              <div className='p-1 hover:scale-110 transition duration-250'><DiLinux/><span className='text-sm pl-2'>Linux</span></div>
+              <div className='p-1 hover:scale-110 transition duration-250'><FaJava/>
+              <span className='pl-2 text-sm'>Java</span></div>
+              <div className='p-1 hover:scale-110 transition duration-250'><BiLogoSpringBoot/>
+              <span className='pl-1 text-sm'>Spring</span></div>
+              <div className='p-1 hover:scale-110 transition duration-250'><DiMysql/>
+              <span className='text-sm pl-1'>MySQL</span></div>
+              <div className='p-1 hover:scale-110 transition duration-250'><DiLinux/>
+              <span className='text-sm pl-2'>Linux</span></div>
             </div>
-            <p className='text-md mb-10 py-2 leading-8 text-gray-600 md:text-xl max-w-xl mx-auto md:leading-10  dark:text-gray-300'>
-              I specialize in Web Development, with a particular emphasis on <span className='font-bold  dark:text-white'>backend</span> technologies using <span className='font-bold  dark:text-white'>Java</span> and <span className='font-bold  dark:text-white'>Spring Boot</span>. 
+            <p className='text-md mb-10 py-2 leading-8 text-gray-600 md:text-xl max-w-xl mx-auto md:leading-10  
+            dark:text-gray-300'>
+              I specialize in Web Development, with a particular emphasis on <span className='font-bold  
+              dark:text-white'>backend</span> technologies using <span className='font-bold  dark:text-white'>
+                Java</span> and <span className='font-bold  dark:text-white'>Spring Boot</span>. 
               While my primary expertise lies in this area, I maintain a flexible and expansive approach to learning. 
-              I am continuously exploring and studying various other areas and programming languages, ensuring a well-rounded and adaptable skill set in the ever-evolving field 
+              I am continuously exploring and studying various other areas and programming languages, ensuring a 
+              well-rounded and adaptable skill set in the ever-evolving field 
               of web development.
             </p>
           </div>
         </section>
         <footer class="bg-white dark:bg-gray-950">
             <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-              <span class="text-sm text-gray-600 sm:text-center dark:text-gray-300">2024 <a href="https://kielblock.dev/" class="hover:underline">kielblock.dev</a>
+              <span class="text-sm text-gray-600 sm:text-center dark:text-gray-300"><a href="https://kielblock.dev/" 
+              class="hover:underline">kielblock.dev</a>
             </span>
             <ul class="flex flex-wrap  items-center mt-3 text-sm font-medium text-gray-600 dark:text-gray-300 sm:mt-0">
                 <li>
