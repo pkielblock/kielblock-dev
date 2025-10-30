773093bafb3afb58eb8d1221c668612da6821fbc
Pedro Kielblock <pedro@kielblock.dev>
2025-10-30 16:17:12 -0300

    feat: add vercel analytics and speed observability tool into

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,18 @@
 	import Navbar from '$lib/components/portfolio/Navbar.svelte';
 	import '../app.css';
 	import { ModeWatcher, setMode } from 'mode-watcher';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
 	setMode('dark');
+	injectAnalytics();
+	injectSpeedInsights();
 </script>
 
 <ModeWatcher />
-<div class="relative mx-auto min-h-screen max-w-2xl bg-background px-6 py-12 font-sans antialiased sm:py-24">
+<div
+	class="relative mx-auto min-h-screen max-w-2xl bg-background px-6 py-12 font-sans antialiased sm:py-24"
+>
 	<slot></slot>
 	<Navbar />
 </div>
